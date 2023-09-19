180d450ca2760ec81682d7d5df5eeb3c6137bcbf
Ty_Chhenglong <tychhenglong1@gmail.com>
2023-09-19 16:09:48 +0700

    update

--- Course_Elective/routes/web.php
+++ Course_Elective/routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StudentController;
-use App\Http\Controllers\AcademicYearsController;
 
 /*
 |--------------------------------------------------------------------------
